f6edd3fafe811c168c1f9f7f9529171e76b77997
Frank Albanese <frank@ocavu.com>
2023-04-05 14:03:17 -0600

    updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 app.listen(process.env.PORT || 3000)
